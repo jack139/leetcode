7a439c25352c2180e56ec99c02fe1ba9ff6f4175
gt <jack139@gmail.com>
2022-01-12 18:27:31 +0800

    add p17

---
+++ 17_letter-combinations-of-a-phone-number/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func letterCombinations(digits string) []string {
+    
+}
+
+func main(){    
+	fmt.Println(letterCombinations())
+	fmt.Println(letterCombinations()) 
+	fmt.Println(letterCombinations())
+}
